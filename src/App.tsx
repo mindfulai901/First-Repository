92033a01dbae2f0a33309db747fc3112ca512ebc
mindfulai901 <mindfulai901@gmail.com>
2025-10-26 08:09:20 -0700

    refactor: Remove unnecessary import
    Removes a redundant React import from App.tsx. This import was not being used and did not contribute to the application's functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-
-import React, 'react';
+import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AppContent } from './AppContent';
